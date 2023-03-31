403dc7e1c9ffea36c779348bb22c5aa1b72e2450
smriti09 <smritidas@umass.edu>
2023-03-31 17:30:09 +0000

    updated the pid code for our use

--- code/pid.py
+++ code/pid.py
@@ -6,17 +6,23 @@ def getAllPids():
     psCmd = ['ps', '-e', '-o', 'pid']
     out = subprocess.Popen(psCmd, stdout=subprocess.PIPE).communicate()[0]
     out = ''.join(map(chr,out))
-    #print(out)
+    print("out",type(out))
+    out=out.splitlines()
+    out.pop(0)
     return out
 
 # Return a dictionary mapping process IDs to container names
 def getContProcs():
     pids = getAllPids()
     groupMapping = {}
+    print("pids:",pids[0])
     for pid in pids:
+        pid=pid.strip()
+        print("pid:",str(pid))
         cat = ["cat", "/proc/"+str(pid)+"/cgroup"]
         catOut = str(subprocess.Popen(cat, stdout=subprocess.PIPE).communicate()[0])
-        group = re.findall(r"lxc/([\w-]+)", str(catOut))[0]
+        print("catOut",catOut)
+        group = re.findall(r"memory", str(catOut))[0]
 
         groupMapping[pid] = group
     
